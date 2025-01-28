0cbce84f2b88b4ec1ab017e78f9d390263a939c6
larionov-m-d <larionov-m-d@yandex-team.ru>
2025-01-28 20:11:13 +0300

    update: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.detekt)
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
@@ -8,4 +9,18 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+}
+
+detekt {
+    parallel = true
+    source.from(
+        files(
+            fileTree(baseDir = rootDir) {
+                include("**/src/**/*.kt")
+            }
+        )
+    )
+    baseline = File(rootDir, "/config/detekt/baseline.xml")
+    config.from(files(File(rootDir, "/config/detekt/detekt.yml")))
+    ignoredVariants = listOf("release")
 }
